ada7fdcc75d7029bb1d67f63190cde927dac2815
nikhil-kamboj420 <kambojnikhil44@gmail.com>
2025-07-16 01:34:12 +0530

    add ES_moulde/

---
+++ modules/ES_moulde/app.js
@@ -0,0 +1,16 @@
+import * as math from "./math.js";
+import {add,sub,mul,dev} from "./math.js";
+import {PI} from "./math.js"
+
+console.log(math.add(4,6));
+console.log(math.sub(4,6));
+console.log(math.mul(4,6));
+console.log(math.dev(4,6));
+
+console.log(PI);
+
+console.log(add(2,6));
+console.log(sub(2,6));
+console.log(mul(2,6));
+console.log(dev(2,6));
+
---
+++ modules/ES_moulde/math.js
@@ -0,0 +1,15 @@
+const add = (a,b)=>{
+return a + b;
+}
+const sub = (a,b)=>{
+return a - b;
+}
+const mul = (a,b)=>{
+return a * b;
+}
+const dev = (a,b)=>{
+return a / b;
+}
+export const PI = 3.14;
+
+export{add,sub,mul,dev};
